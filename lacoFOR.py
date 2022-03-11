a55759c11d039d2d32a9806b8c37358eee14b534
Leandro Afonso da Silva de Almeida <79644610+LeandroASAlmeida@users.noreply.github.com>
2022-03-10 21:54:04 -0300

    tabuada

--- lacoFOR.py
+++ lacoFOR.py
@@ -1,1 +1,14 @@
-for valor in range()
+'''for valor in range(1, 11, 3):
+    print(valor)
+
+######'''
+
+
+valor = int(input('INFORME UM VALOR INTEIRO DE 1 A 10: '))
+print('TABUADA DE ' + str(valor))
+
+for cont in range(1, 11):
+    print(str(valor) + 'x' + str(cont) + '=' + str(valor*cont))
+
+
+
